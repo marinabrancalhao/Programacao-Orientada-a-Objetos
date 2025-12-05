2917f94eaf30f614f82e2796f8ede45c1d91db1b
Marina <mahbrancalhao@gmail.com>
2025-12-05 19:22:30 -0300

    círculo

---
+++ Unidade 01/Atividade 01/Exercicio 01/Circulo.java
@@ -0,0 +1,9 @@
+/*
+Imagine que você está criando um aplicativo para uma loja de materiais de arte.
+Crie uma classe Círculo que represente um círculo, incluindo atributos como raio e área, e implemente métodos para calcular a área e o perímetro.
+Isso ajudará os artistas a entender melhor as medidas de seus trabalhos!
+*/
+
+public class Circulo {
+    
+}
