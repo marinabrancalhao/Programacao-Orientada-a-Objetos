e242d0a01268adb46c2aab336f40ba99c9221173
Marina <mahbrancalhao@gmail.com>
2025-12-06 16:19:10 -0300

    retângulo

--- Unidade 01/Atividade 01/Exercicio 04/Retangulo.java
+++ Unidade 01/Atividade 01/Exercicio 04/Retangulo.java
@@ -4,6 +4,44 @@ Implemente uma classe Retângulo com atributos como comprimento e largura, e mé
 Esse conhecimento será útil para muitos!
 */
 
+import java.util.Scanner;
+
 public class Retangulo {
+
+    private double comprimento;
+    private double largura;
+
+    Retangulo(double comprimento, double largura){
+
+        this.comprimento = comprimento;
+        this.largura = largura;
+
+    }
+
+    public double calcularArea(double comprimento, double largura){
+
+        double area = comprimento * largura;
+
+        return area;
+    }
+
+    public static void main(String[] args){ 
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.err.println("== Cáculo de Área ==");
+
+        System.out.print("\nDigite o comprimento do terreno: ");
+        double comprimento = scanner.nextDouble();
+
+        System.out.print("Digite a largura do terreno: ");
+        double largura = scanner.nextDouble();
+
+        Retangulo retangulo = new Retangulo(comprimento, largura);
+
+        System.out.printf("\nÁrea do terreno: %.2f m²", retangulo.calcularArea(comprimento, largura));
+
+        scanner.close();
+    }
     
 }
