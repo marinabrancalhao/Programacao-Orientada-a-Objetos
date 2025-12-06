8800b8556a4a452584a6cadc66b24e0440b28707
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:33:49 -0300

    livro

---
+++ Unidade 01/Atividade 01/Exercicio 03/Livro.java
@@ -0,0 +1,9 @@
+/*
+Você está ajudando a montar uma biblioteca digital.
+Crie uma classe Livro com atributos como título, autor e número de páginas, além de um método para exibir detalhes do livro.
+Assim, os usuários poderão explorar suas leituras de forma mais fácil.
+*/
+
+public class Livro {
+    
+}
