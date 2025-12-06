c826156f494d590200d44b40bff5aeb942355ece
Marina <mahbrancalhao@gmail.com>
2025-12-06 16:45:18 -0300

    produto

--- Unidade 01/Atividade 01/Exercicio 06/Produto.java
+++ Unidade 01/Atividade 01/Exercicio 06/Produto.java
@@ -4,6 +4,40 @@ Crie uma classe Produto com atributos como nome e preço, e um método para calc
 Isso ajudará os clientes a economizar em suas compras!
 */
 
+import java.util.Scanner;
+
 public class Produto {
     
+    private String nome;
+    private double preco;
+
+    Produto(String nome, double preco) {
+
+        this.nome = nome;
+        this.preco = preco;
+
+    }
+
+    public double calcularDesconto(double preco){
+
+        double desconto = 0.10;
+        double precoComDesconto = preco - (preco * desconto);
+
+        return precoComDesconto;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do produto: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o preço para aplicar desconto: R$ ");
+        double preco = scanner.nextDouble();
+        
+        Produto produto = new Produto(nome, preco);
+
+        System.out.printf("\nPreço com desconto: R$ %.2f%n",produto.calcularDesconto(preco));
+    }
 }
