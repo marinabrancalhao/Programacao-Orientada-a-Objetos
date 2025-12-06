a3f3d0e3e2aa7e1046f277f87026e955c52133a5
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:15:00 -0300

    circulo

--- Unidade 01/Atividade 01/Exercicio 01/Circulo.java
+++ Unidade 01/Atividade 01/Exercicio 01/Circulo.java
@@ -4,6 +4,41 @@ Crie uma classe Círculo que represente um círculo, incluindo atributos como ra
 Isso ajudará os artistas a entender melhor as medidas de seus trabalhos!
 */
 
+import java.util.Scanner;
+
 public class Circulo {
+    
+    private double raio;
+
+    public Circulo(double raio){
+
+        this.raio = raio;
+    }
+
+    public double calcularArea(){
+
+        return Math.PI * raio * raio;
+
+    }
+
+    public double calcularPerimetro(){
+
+        return 2 * Math.PI * raio;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o valor do raio do círculo para calcular área e perímetro: ");
+        double raio = scanner.nextDouble();
+
+        Circulo circulo = new Circulo(raio);
+
+        System.out.printf("Área: %.2f%n", circulo.calcularArea());
+        System.out.printf("Perímetro: %.2f%n", circulo.calcularPerimetro());
+
+        scanner.close();
+    }
     
 }
