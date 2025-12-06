46aaa00f273409235cb1217839f740bd8f157921
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:34:17 -0300

    animal

---
+++ Unidade 01/Atividade 01/Exercicio 05/Animal.java
@@ -0,0 +1,9 @@
+/*
+Imagine que você está criando um aplicativo para um zoológico.
+Desenvolva uma classe Animal com características como nome e tipo de alimentação, além de um método que permita ao animal emitir um som característico.
+Isso tornará o app mais interativo!
+*/
+
+public class Animal {
+    
+}
