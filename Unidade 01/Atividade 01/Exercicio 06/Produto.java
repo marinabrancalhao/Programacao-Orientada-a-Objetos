7ad0cf8b8b5659cb6164653b69821f0a27f16861
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:34:30 -0300

    produto

---
+++ Unidade 01/Atividade 01/Exercicio 06/Produto.java
@@ -0,0 +1,9 @@
+/*
+Você está criando um sistema de e-commerce.
+Crie uma classe Produto com atributos como nome e preço, e um método para calcular o preço com desconto.
+Isso ajudará os clientes a economizar em suas compras!
+*/
+
+public class Produto {
+    
+}
