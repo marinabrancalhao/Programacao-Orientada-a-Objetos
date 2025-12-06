c58797feee5736dc84a59bfa98c07def5d46a02c
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:34:52 -0300

    triângulo

---
+++ Unidade 01/Atividade 01/Exercicio 07/Triangulo.java
@@ -0,0 +1,9 @@
+/*
+Pense em um programa educacional que ensina geometria.
+Implemente uma classe Triângulo com atributos como base e altura, e um método para calcular a área.
+Essa prática será excelente para os alunos!
+*/
+
+public class Triangulo {
+    
+}
