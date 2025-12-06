ecb2f61c0dc305ec4afbdb0983960eacd7bcf578
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:34:02 -0300

    retângulo

---
+++ Unidade 01/Atividade 01/Exercicio 04/Retangulo.java
@@ -0,0 +1,9 @@
+/*
+Considere a criação de um programa para calcular áreas de terrenos.
+Implemente uma classe Retângulo com atributos como comprimento e largura, e métodos para calcular a área e o perímetro.
+Esse conhecimento será útil para muitos!
+*/
+
+public class Retangulo {
+    
+}
