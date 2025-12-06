b6691d3f36e1554c71319bdcb5b8e5f4c99a571b
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:13:08 -0300

    criando classes

---
+++ Unidade 01/Atividade 01/Exercicio 09/Carro.java
@@ -0,0 +1,9 @@
+/*
+Agora, considere a criação de um simulador de carros.
+Crie uma classe Carro com atributos como marca, modelo e ano, e métodos para ligar, desligar e acelerar.
+Isso tornará a simulação mais realista!
+*/
+
+public class Carro {
+    
+}
