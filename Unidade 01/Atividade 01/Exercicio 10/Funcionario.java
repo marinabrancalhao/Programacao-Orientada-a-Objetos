efc3040f7378604b4c2e525c3714a4fb1f1b3750
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:46:23 -0300

    funcionário

---
+++ Unidade 01/Atividade 01/Exercicio 10/Funcionario.java
@@ -0,0 +1,9 @@
+/*
+Por fim, pense em um sistema de recursos humanos.
+Implemente uma classe Funcionário com características como nome, cargo e salário, e um método para calcular aumento de salário.
+Isso ajudará a gerenciar a equipe de forma eficaz!
+*/
+
+public class Funcionario {
+    
+}
