261246a8f637a112397b51ee5d0c4b7f0fe46eb3
Marina <mahbrancalhao@gmail.com>
2025-12-06 13:46:13 -0300

    conta bancária

---
+++ Unidade 01/Atividade 01/Exercicio 08/ContaBancaria.java
@@ -0,0 +1,9 @@
+/*
+Imagine que você está desenvolvendo um aplicativo de finanças pessoais.
+Crie uma classe ContaBancária com atributos como saldo e número da conta, e métodos para depositar e sacar.
+Isso ajudará os usuários a gerenciar suas economias!
+*/
+
+public class ContaBancaria {
+    
+}
