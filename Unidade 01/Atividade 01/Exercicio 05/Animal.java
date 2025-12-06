7a7418312964f8f04117740dbef9d244eac2aad6
Marina <mahbrancalhao@gmail.com>
2025-12-06 16:34:02 -0300

    animal

--- Unidade 01/Atividade 01/Exercicio 05/Animal.java
+++ Unidade 01/Atividade 01/Exercicio 05/Animal.java
@@ -5,5 +5,28 @@ Isso tornará o app mais interativo!
 */
 
 public class Animal {
+
+    private String nome;
+    private String tipoAlimentacao;
+
+    Animal(String nome, String tipoAlimentacao){
+
+        this.nome = nome;
+        this.tipoAlimentacao = tipoAlimentacao;
+
+    }
+
+    public void emitirSom(){
+
+        System.out.println("O animal " + nome + " está emitindo um som.");
+
+    }
+
+    public static void main(String[] args) {
+        
+        Animal animal = new Animal("Panda", "Bambu");
+
+        animal.emitirSom();
+    }
     
 }
