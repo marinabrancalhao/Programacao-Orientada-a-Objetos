d28db5595a08dc95a223b217f18711f309816f55
Marina <mahbrancalhao@gmail.com>
2025-12-06 14:29:04 -0300

    livros

---
+++ Unidade 01/Atividade 01/Exercicio 03/GerenciarLivros.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GerenciarLivros {
+
+    private ArrayList<Livro> biblioteca;
+
+    public GerenciarLivros(){
+
+        biblioteca = new ArrayList<>();
+    }
+
+    public void cadastrarLivro(Livro livro){
+        biblioteca.add(livro);
+    }
+
+    public void exibirLivros(){
+        for (Livro livro : biblioteca) {
+            System.out.println("Título: " + livro.getTitulo());
+            System.out.println("Autor: " + livro.getAutor());
+            System.out.println("Número de páginas: " + livro.getNumeroPaginas());
+            System.out.println("---------------------------------------------------");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        GerenciarLivros biblioteca = new GerenciarLivros();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean continuar = true;
+        while (continuar) {
+        
+            System.out.println("\n== Biblioteca Online ==");
+            System.out.println("1. Cadastrar livros");
+            System.out.println("2. Exibir livros cadastrados");
+            System.out.println("0. Sair");
+            System.out.print("\nDigite a opção desejada: ");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+
+                    System.out.print("\nTítulo: ");
+                    String titulo = scanner.nextLine();
+
+                    System.out.print("Autor: ");
+                    String autor = scanner.nextLine();
+
+                    System.out.print("Número de páginas: ");
+                    int numeroPaginas = scanner.nextInt();
+                    scanner.nextLine();
+
+                    biblioteca.cadastrarLivro(new Livro(titulo, autor, numeroPaginas));
+
+                    System.out.println("\nLivro cadastrado com sucesso!");
+
+                    break;
+
+                case 2:
+
+                    System.out.println("\n--- Livros Cadastrados ---\n");
+                    biblioteca.exibirLivros();
+
+                    break;
+            
+                case 0 :
+
+                    System.out.println("Programa encerrado...");
+
+                    continuar = false;
+
+                    break;
+
+                default:
+
+                    System.out.println("Opção inválida, tente novamente...");
+
+                    break;
+            }
+        }
+
+
+
+    }
+    
+}
--- Unidade 01/Atividade 01/Exercicio 03/Livro.java
+++ Unidade 01/Atividade 01/Exercicio 03/Livro.java
@@ -5,5 +5,27 @@ Assim, os usuários poderão explorar suas leituras de forma mais fácil.
 */
 
 public class Livro {
-    
+
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+
+    public Livro(String titulo, String autor, int numeroPaginas){
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
 }
