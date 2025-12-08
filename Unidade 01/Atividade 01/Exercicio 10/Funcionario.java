29f9444ac9d8cc6ed8be4a7b3354d18911b3f150
Marina <mahbrancalhao@gmail.com>
2025-12-08 14:52:46 -0300

    funcionário

--- Unidade 01/Atividade 01/Exercicio 10/Funcionario.java
+++ Unidade 01/Atividade 01/Exercicio 10/Funcionario.java
@@ -4,6 +4,51 @@ Implemente uma classe Funcionário com características como nome, cargo e salá
 Isso ajudará a gerenciar a equipe de forma eficaz!
 */
 
+import java.util.Scanner;
+
 public class Funcionario {
     
+    private String nome;
+    private String cargo;
+    private double salario;
+    private double aumento;
+
+    Funcionario(String nome, String cargo, double salario, double aumento){
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.aumento = aumento;
+    }
+
+    public double calcularAumento(double salario){
+
+        return salario += salario * (aumento / 100) ;
+
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do funcionário: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o cargo: ");
+        String cargo = scanner.nextLine();
+
+        System.out.print("Qual o salário atual? ");
+        double salario = scanner.nextDouble();
+
+        System.out.print("Em quantos % será o aumento? ");
+        double aumento = scanner.nextDouble();
+
+        Funcionario funcionario = new Funcionario(nome, cargo, salario, aumento);
+
+        System.out.printf("\nSalário atualizado: %.2f%n", funcionario.calcularAumento(salario));
+
+        scanner.close();
+
+
+
+    }
 }
