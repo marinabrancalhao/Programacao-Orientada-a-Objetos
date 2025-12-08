3778f0a5a881f1b4b1a92c60c05a9a4b22abf067
Marina <mahbrancalhao@gmail.com>
2025-12-08 12:05:41 -0300

    carro

--- Unidade 01/Atividade 01/Exercicio 09/Carro.java
+++ Unidade 01/Atividade 01/Exercicio 09/Carro.java
@@ -5,5 +5,55 @@ Isso tornará a simulação mais realista!
 */
 
 public class Carro {
+
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    Carro(String marca, String modelo, int ano){
+
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public String ligar(){
+
+        return "O carro " + modelo + " " + ano + " está ligado.";
+    }
+ 
+    public String acelerar(){
+
+        return "Acelerando o " + modelo + "...";
+    }
+
+    public String desligar(){
+
+        return "O carro " + modelo + " " + ano + " está desligado.";
+    }
+
+    public static void main(String[] args) {
+        
+        Carro carro = new Carro("Volkswagen", "Fusca", 1975);
+        Carro carro2 = new Carro("Fiat", "Uno", 2015);
+        Carro carro3 = new Carro("Nissan", "Kicks", 2021);
+
+        System.out.println(carro.ligar());
+        System.out.println(carro2.acelerar());
+        System.out.println(carro3.desligar());
+    }
     
 }
